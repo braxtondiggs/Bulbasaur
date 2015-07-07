fa7f8a551e029da1244a6885b29bcda59dc6e52c
Braxton Diggs <braxtondiggs@gmail.com>
2015-07-07 02:16:05 -0400

    Convert Site to AngularJS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,23 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name bulbasaurApp
- * @description
- * # bulbasaurApp
- *
- * Main module of the application.
- */
 angular
-  .module('bulbasaurApp', [
-    'ngAnimate',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('bulbasaur', [
+        'ngAnimate',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+        $locationProvider.html5Mode(true);
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,71 @@
 'use strict';
+/*global $:false*/
+function onLoad(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('[data-js=scroll-show]').addClass('scroll-show').fadeIn('slow');
+        } else {
+            $('[data-js=scroll-show]').removeClass('scroll-show').fadeOut('slow');
+        }
+    });
+    var navBarYOffset = $("#work").offset().top;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > navBarYOffset) {
+            $('body').addClass('navbar-fixated');
+        } else {
+            $('body').removeClass('navbar-fixated');
+        }
+    });
+    $(".navbar-toggle").click(function() {
+        $(".navbar-collapse").slideToggle();
+    });
+    $('.scroll').click(function(e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+    });
 
-/**
- * @ngdoc function
- * @name bulbasaurApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the bulbasaurApp
- */
-angular.module('bulbasaurApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('.menu').fadeIn();
+
+        } else {
+            $('.menu').fadeOut();
+        }
+        if ($(this).scrollTop() > 550) {
+            $('.second').addClass('fade-in2');
+        }
+        if ($(this).scrollTop() > 1000) {
+            $('.exp').addClass('left-new');
+        }
+        if ($(this).scrollTop() > 2000) {
+            $('.works').addClass('left-new2');
+        }
+    });
+    /*$('#contact-container button').on('click', function() {
+        $.ajax({
+            type: "POST",
+            url: "index.php/email",
+            data: $('#contact-form').serialize()
+        }).done(function() {
+            $('#contact-form').fadeOut('slow', function() {
+                $('#confirmation').fadeIn('slow');
+            });
+        });
+        return false;
+    });*/
+}
+angular.module('bulbasaur').controller('MainCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+	$timeout(function() {
+		onLoad();
+	}, 500);
+	var time = new Date();
+	$scope.myForm = {};
+	$scope.age = parseInt(time.getFullYear() - 1988, 10);
+	$scope.submit = function() {
+		$('#contact-form').fadeOut('slow', function() {
+            $('#confirmation').fadeIn('slow');
+        });
+	};
+}]);
