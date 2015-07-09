119a912eabd4efed03f8cc7ab13d5b39a1045355
Braxton Diggs <braxtondiggs@gmail.com>
2015-07-09 13:37:43 -0400

    Added Server-Side Logging

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,8 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
+  require('newrelic');
+
   // Automatically load required Grunt tasks
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
