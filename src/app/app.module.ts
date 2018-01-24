cc30b53ff25cb82622e163bb8e0892d61c8f0e71
Braxton Diggs <braxtondiggs@gmail.com>
2018-01-24 14:33:40 -0500

    Add header, convert to scss, add linter & add new packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
-
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    FlexLayoutModule,
+    MaterialModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    LoadingBarHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
