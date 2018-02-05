700396bd99319995f3146e9cf2d4cdd4c708039d
Braxton Diggs <braxtondiggs@gmail.com>
2018-02-05 13:22:02 -0500

    fix path & pipe

--- src/app/pipes/skill.pipe.ts
+++ src/app/pipes/skill.pipe.ts
@@ -15,10 +15,8 @@ export class SkillPipe implements PipeTransform {
       return 80 + wildcard;
     } else if (value < 30) {
       return 90 + wildcard;
-    } else if (value < 40) {
+    } else if (value > 40) {
       return 100;
-    } else {
-      return value;
     }
   }
 }
