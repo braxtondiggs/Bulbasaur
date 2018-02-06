f1408cfbf6d9ae5c4fc15984fde9097d3c4afc22
Braxton Diggs <braxtondiggs@gmail.com>
2018-02-05 22:56:17 -0500

    Move Angular CLI to dependencies for Heroku build

--- server/index.js
+++ server/index.js
@@ -1,13 +1,6 @@
 'use strict';
-
-// Set default node environment to development
-// eslint-disable-next-line no-process-env
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if (env === 'development' || env === 'test') {
-  // Register the Babel require hook
-  require('babel-register');
-}
+// Register the Babel require hook
+require('babel-register');
 
 // Export the application
 exports = module.exports = require('./app');
