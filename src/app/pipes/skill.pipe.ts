0e26f7ab3ec2d2ace0e94e5c862f4b35edd6e838
Braxton Diggs <braxtondiggs@gmail.com>
2018-02-07 12:08:33 -0500

    reduce number of returns

--- src/app/pipes/skill.pipe.ts
+++ src/app/pipes/skill.pipe.ts
@@ -9,12 +9,8 @@ export class SkillPipe implements PipeTransform {
     const wildcard: number = _.toNumber(_.toString(value).slice(-1));
     if (value <= 0) {
       return 60 + wildcard;
-    } else if (value < 10) {
-      return 70 + wildcard;
-    } else if (value < 20) {
-      return 80 + wildcard;
-    } else if (value < 30) {
-      return 90 + wildcard;
+    } else if (value <= 30) {
+      return 60 + _.ceil((value + 1) / 10) * 10 + wildcard;
     } else {
       return 100;
     }
