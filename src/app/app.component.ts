91f3140da8c1dfbfbfcd229acc39ed9165d56fa7
Braxton Diggs <braxtondiggs@gmail.com>
2020-03-06 13:26:32 -0500

    Add basic example of music & analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component, HostListener, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, ViewEncapsulation, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { GoogleAnalyticsService } from './shared/services';
+import { skip } from 'rxjs/operators';
 import { ITrack } from './shared/model';
+import 'firebase/firestore';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -8,18 +12,32 @@ import { ITrack } from './shared/model';
   styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public scroll: number;
   public track: ITrack;
-  public duration = 0;
-  constructor(public ga: GoogleAnalyticsService) { }
+  public trackLink: SafeResourceUrl;
+  public duration: number;
+  constructor(private afs: AngularFirestore, public ga: GoogleAnalyticsService, private sanitizer: DomSanitizer) { }
 
   @HostListener('window:scroll', ['$event'])
   public onScroll(): void {
     this.scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
   }
 
-  public initTrackFlow() {
+  ngOnInit() {
+    this.afs.collection('tracks', ref => ref.limit(1)).snapshotChanges(['added']).pipe(skip(1)).subscribe((snapshot) => {
+      this.track = snapshot[0].payload.doc.data() as any;
+      this.trackLink =
+        this.sanitizer.bypassSecurityTrustResourceUrl(`https://open.spotify.com/embed/track/${this.track.id}`);
+      this.initTrackFlow();
+    });
+  }
+
+  public isScroll(): boolean {
+    return this.scroll > 75;
+  }
+
+  private initTrackFlow() {
     let x = 0;
     setTimeout(() => {
       this.track = null;
@@ -31,9 +49,5 @@ export class AppComponent {
       x++;
       this.duration = (x / (this.track.duration / 250)) * 100;
     }, 250);
-  }
-
-  public isScroll(): boolean {
-    return this.scroll > 75;
   }
 }
