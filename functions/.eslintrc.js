e041bbaf43ddcbec5069a97181980acb00abd050
Braxton Diggs <braxtondiggs@gmail.com>
2021-05-10 02:37:12 -0400

    Add Instagram buckets

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,7 +28,6 @@ module.exports = {
     "@typescript-eslint/prefer-for-of": "warn",
     "@typescript-eslint/triple-slash-reference": "error",
     "@typescript-eslint/unified-signatures": "warn",
-    "comma-dangle": ["error", "always-multiline"],
     "constructor-super": "error",
     "import/no-deprecated": "warn",
     "import/no-extraneous-dependencies": "error",
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as cors from 'cors';
 import * as express from 'express';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -11,11 +13,31 @@ const app = express();
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
-app.post('/instagram', (request: functions.Request, response: functions.Response) => {
+app.post('/instagram', async (request: functions.Request, response: functions.Response) => {
   if (request.body['Url'] && request.body['SourceUrl']) {
     const id = request.body['Url'].split('/').filter(Boolean).pop();
     if (id) {
-      db.doc(`instagram/${id}`).set({ ...request.body, CreatedAt: new Date() }).then(() => {
+      const image = await axios.get(request.body['SourceUrl'], { responseType: 'arraybuffer' });
+      const contentType = image.headers['content-type'];
+      const buffer = Buffer.from(image.data).toString('base64');
+      const imageB64 = `data:${contentType};base64,${buffer}`;
+      const type = contentType.split('/').splice(-1).join();
+      const file = admin.storage().bucket().file(`instagram/${id}.${type}`);
+      await file.save(imageB64, {
+        metadata: {
+          contentType,
+          metadata: {
+            firebaseStorageDownloadTokens: uuidv4()
+          }
+        },
+        public: true,
+        validation: 'md5',
+        predefinedAcl: 'publicRead'
+      });
+
+      const [metaData] = await file.getMetadata();
+
+      db.doc(`instagram/${id}`).set({ ...request.body, SourceUrl: metaData.mediaLink, CreatedAt: new Date() }).then(() => {
         response.send(request.body);
       }).catch((error) => {
         response.status(500).send(`Error: ${error.toString()}`);
